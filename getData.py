f3321484c0b6d85c6f8cc054cc7d2adfe87ca1c6
kiwidamien <djmartin@ucdavis.edu>
2017-06-18 18:15:55 -0700

    Converted popup to template; changed sql schema so long and lat were read in as integers

--- getData.py
+++ getData.py
@@ -1,63 +1,9 @@
 import sqlite3 as db
 import folium
+from flask import render_template
 
 data_src = "data/landslides.sql"
 
-
-my_popup_html="""
-<!DOCTYPE html>
-<html><head>
-<style>
-table {{ width: 100%;}}
-
-    table, th, td {{
-      border: 1px solid black;
-      border-collapse: collapse;
-      }}
-
-    th, td {{
-      padding: 5px;
-      text-align: left;
-  }}
-  table#t01 tr:nth-child(odd) {{
-      background-color: #8e8;
-  }}
-  table#t01 tr:nth-child(even) {{
-      background-color:#fff;
-  }}
-  </style>
-  </head>
-  <body>
-    <table id="t01">
-      <tr> <td>Country</td> <td>{country}</td> </tr>
-      <tr> <td>Location</td> <td>{nearest_places}</td> </tr>
-      <tr> <td>date</td> <td>{date}</td></tr>
-      <tr> <td>Landslide type</td> <td>{landslide_type}</td></tr>
-      <tr> <td>Trigger</td> <td>{trigger}</td> </tr>
-      <tr> <td>Fatalities</td> <td>{fatalities}</td> </tr>
-      <tr> <td>Landslide size</td> <td>{landslide_size}</td></tr>
-      <tr> <td>Latitude</td> <td>{latitude}</td></tr>
-      <tr> <td>Longitude</td> <td>{longitude}</td></tr>
-    </table>
-
-  </body>
-  </html>
-  """
-
-"""
-for _, row in store_data.iterrows():
-  try:
-    tableFrame = folium.IFrame(html = my_popup_html.format(**row),width =500, height = 300)
-    popupTable = folium.Popup(tableFrame, max_width= 500)
-    folium.CircleMarker(
-      location = [row["latitude"], row["longitude"]],
-      radius = 5,
-      fill_color = "Brown",
-      popup  = popupTable
-      ).add_to(landslide_map)
-  except:
-    pass
-"""
 
 def getData( bounding_box, limit=600 ):
     conn = db.connect(data_src)
@@ -79,7 +25,7 @@ def getData( bounding_box, limit=600 ):
     for data in cursor.execute(query):
         try:
             N = N+1
-            tableFrame = folium.IFrame(html = my_popup_html.format(**data), width=500, height=300)
+            tableFrame = folium.IFrame(html = render_template("popup.html", column = data), width=500, height=300)
             popupTable = folium.Popup(tableFrame, max_width = 500)
             folium.CircleMarker(
                 location = [data["latitude"], data["longitude"]],
--- landslide_server.py
+++ landslide_server.py
@@ -5,69 +5,20 @@ import folium
 import os
 import geocoder
 
+from getData import getData
 
-store_data = pd.read_csv("global_landslides.csv")
-landslide_map = folium.Map(tiles="Stamen Terrain", zoom_start = 6)
-store_data = store_data[:100]
+#store_data = pd.read_csv("global_landslides.csv")
+#landslide_map = folium.Map(tiles="Stamen Terrain", zoom_start = 6)
+#store_data = store_data[:100]
 
 app = Flask(__name__)
-
-my_popup_html="""
-<!DOCTYPE html>
-<html><head>
-<style>
-table {{ width: 100%;}}
-
-    table, th, td {{
-      border: 1px solid black;
-      border-collapse: collapse;
-      }}
-
-    th, td {{
-      padding: 5px;
-      text-align: left;
-  }}
-  table#t01 tr:nth-child(odd) {{
-      background-color: #8e8;
-  }}
-  table#t01 tr:nth-child(even) {{
-      background-color:#fff;
-  }}
-  </style>
-  </head>
-  <body>
-    <table id="t01">
-      <tr> <td>Country</td> <td>{country}</td> </tr>
-      <tr> <td>Location</td> <td>{nearest_places}</td> </tr>
-      <tr> <td>date</td> <td>{date}</td></tr>
-      <tr> <td>Landslide type</td> <td>{landslide_type}</td></tr>
-      <tr> <td>Trigger</td> <td>{trigger}</td> </tr>
-      <tr> <td>Fatalities</td> <td>{fatalities}</td> </tr>
-      <tr> <td>Landslide size</td> <td>{landslide_size}</td></tr>
-      <tr> <td>Latitude</td> <td>{latitude}</td></tr>
-      <tr> <td>Longitude</td> <td>{longitude}</td></tr>
-    </table>
-
-  </body>
-  </html>
-  """
-
-for _, row in store_data.iterrows():
-  try:
-    tableFrame = folium.IFrame(html = my_popup_html.format(**row),width =500, height = 300)
-    popupTable = folium.Popup(tableFrame, max_width= 500)
-    folium.CircleMarker(
-      location = [row["latitude"], row["longitude"]],
-      radius = 5,
-      fill_color = "Brown",
-      popup  = popupTable
-      ).add_to(landslide_map)
-  except:
-    pass
+currentMap = [None]
 
 @app.route("/",)
 def map():
-  return render_template("layout.html", view_map = landslide_map)
+    landslide_map = getData([[-180,-70],[180,80]])
+    currentMap[0] = landslide_map
+    return render_template("layout.html", view_map = landslide_map)
 
 @app.route("/location/<name>")
 def relocate(name):
@@ -78,14 +29,15 @@ def relocate(name):
         # this is a better solution than always giving a +/ 10 degrees
         # The BB is a little narrow, so we
         if g.status == 'OK':
-          landslide_map.fit_bounds( [g.southwest, g.northeast] )
+          landslide_map = getData( [g.southwest, g.northeast] )
+          currentMap[0] = landslide_map
           return render_template("layout.html",
                                  view_map = landslide_map,
                                  location = name)
         else:
           print "Going to the default"
           return render_template("layout.html",
-                                 view_map = landslide_map,
+                                 view_map = currentMap[0],
                                  location = 'Not found',
                                  error    = "Could not locate {}".format(name))
 
